b42a9975fdc76511d5d2c8550f0106d21b17f15a
Vizenp <kalinskyvi@gmail.com>
2022-04-05 00:09:11 +0300

    test.py

--- src/test.py
+++ src/test.py
@@ -1,10 +1,18 @@
-a = ['a', 'b', 'c']
-b = []
-for l1 in a:
-  for l2 in a:
-    for l3 in a:
-      print(l1 + l2 + l3)
-      if(l1 == l2 or l1 == l3 or l2 == l3):
-        continue
-      b.append(l1+l2+l3)
-print(b)
+import random
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+smallLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+mail = ""
+password = ""
+name = ""
+lastName = ""
+for i in range(10):
+    temp = random.randint(1, 3)
+    if temp == 1:
+        password += random.choice(letters)
+    elif temp == 2:
+        password += random.choice(smallLetters)
+    else:
+        password += str(random.choice(numbers))
+lastName = mail = name = password

1983b538bf3bb95509e21f24d995877b65f7e4f7
Vizenp <kalinskyvi@gmail.com>
2022-04-13 00:48:31 +0300

    quadratic equation

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -4,24 +4,52 @@ import java.util.Scanner;
 public class QuadraticEquation {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
-        solveEquration(a, b, c);
+        solveEquation();
     }
 
-    public static void solveEquration(int a, int b, int c){
+    public static void solveEquation(){
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("a = ");
+        a = scanner.nextInt();
+        System.out.print("b = ");
+        b = scanner.nextInt();
+        System.out.print("c = ");
+        c = scanner.nextInt();
+        if(b % 2 != 0) solveUncountable(a, b, c);
+        else solveCountable(a, b, c);
+    }
+    private static void solveUncountable(int a, int b, int c){
         int d = b * b - 4 * a * c;
-        //if (b % 2 != 0) d = b * b - 4 * a * c;
-        //else d = (b / 2) * (b / 2) - a * c;
 
         //ВЫВОД ДИСКРИМИНАНТА
-        if(-4 * a * c < 0) System.out.printf("D = %d%d = %d \n", b * b, -4 * a * c, b * b - 4 * a * c);
-        else System.out.printf("D = %d + %d = %d \n", b * b, -4 * a * c, b * b - 4 * a * c);
+        if(-4 * a * c < 0) System.out.printf("D = %d%d = %d \n", b * b, -4 * a * c, d);
+        else System.out.printf("D = %d + %d = %d \n", b * b, -4 * a * c, d);
 
         //ВЫВОД РЕШЕНИЙ
         if(d > 0){
             System.out.printf("x1 = %d%d / %d = %s \n", -b, (int)-Math.sqrt(d), 2 * a, (-b - Math.sqrt(d)) / (2 * a));
             System.out.printf("x2 = %d+%d / %d = %s \n", -b, (int)+Math.sqrt(d), 2 * a, (-b + Math.sqrt(d)) / (2 * a));
+        }
+        else if(d == 0){
+            System.out.printf("x1 = %d%d / %d = %s \n", -b, (int)-Math.sqrt(d), 2 * a, -b / (2 * a));
+        }
+        else {
+            System.out.println("No solves");
+        }
+    }
+    private static void solveCountable(int a, int b, int c){
+        int k = b/2;
+        int d = k * k - a * c;
+
+        //ВЫВОД ДИСКРИМИНАНТА
+        if(-a * c < 0) System.out.printf("D = %d%d = %d \n", k * k, - a * c, d);
+        else System.out.printf("D = %d + %d = %d \n", k * k, - a * c, d);
+
+        //ВЫВОД РЕШЕНИЙ
+        if(d > 0){
+            System.out.printf("x1 = %d%d / %d = %s \n", -k, (int)-Math.sqrt(d), a, (-k - Math.sqrt(d)) / (a));
+            System.out.printf("x2 = %d+%d / %d = %s \n", -k, (int)+Math.sqrt(d), a, (-k + Math.sqrt(d)) / (a));
         }
         else if(d == 0){
             System.out.printf("x1 = %d%d / %d = %s \n", -b, (int)-Math.sqrt(d), 2 * a, -b / (2 * a));

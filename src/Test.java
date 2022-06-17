f24a04a8462c832af4d38f701ad497b83f124631
Vizenp <kalinskyvi@gmail.com>
2022-06-17 10:10:09 +0300

    Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,60 @@
-import java.io.*;
+import java.util.Scanner;
+
+import javax.sql.rowset.CachedRowSet;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        int count = 0;
-        while (true) {
-            int b = System.in.read();
-            System.out.println(b);
-            if (b == -1) break;
-            count++;
-        }
-        System.out.println(count);
+    public static void main(String[] args) {
+        
+    }
+}
+
+abstract class Person {
+    int age;
+    String name;
+    boolean isMale;
+
+    public Person(String name, int age, boolean isMale) {
+
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void sleep() {
+        System.out.println(this.name + " спит");
+    }
+
+    void eat() {
+        System.out.println(this.name + " ест");
+    }
+
+    void haveASex(Person p) throws InPeriod {
+
+    }
+}
+
+class Male extends Person {
+
+    public Male(String name, int age, boolean isMale) {
+        super(name, age, isMale);
+
+    }
+
+}
+
+class Female extends Person {
+    boolean InPeriod;
+
+    public Female(String name, int age, boolean isMale) {
+        super(name, age, isMale);
+        // TODO Auto-generated constructor stub
+    }
+
+}
+
+class InPeriod extends Exception {
+    public InPeriod() {
+        super("У неё месяки... не получится");
     }
 }
